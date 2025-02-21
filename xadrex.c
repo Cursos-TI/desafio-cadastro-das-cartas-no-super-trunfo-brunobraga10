aa9c09f518affc158998e658b020113bbc213a89
brunobraga10 <brunobragacom9@gmail.com>
2025-02-21 07:30:02 -0300

    Update xadrex.c

--- xadrex.c
+++ xadrex.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-
-//variaveis
+// Variaveis
 char codigoEstado;  // A, B, C, ..., H
 int numeroCidade;   // 1, 2, 3, 4
 char nome[50];
@@ -12,99 +11,81 @@ float area;
 double pib;
 int pontosTuristicos;
 
-
-//printf e scanf para exibir opções para escolha dos status das cartas e depois exibir
-
-//carta 1
+// Carta 1
 printf("***CIDADE 1***: \n");
 
-//codigo cidade A01
-printf("digite codigo da cidade(A,B,C,...H): \n");
-scanf("%c",&codigoEstado);
+// Codigo da cidade A01
+printf("Digite o codigo da cidade (A, B, C, ..., H): \n");
+scanf(" %c", &codigoEstado); 
+printf("Codigo da cidade: %c \n", codigoEstado);
 
-printf("codigo da cidade:%c \n",codigoEstado);
+// Numero da cidade A01
+printf("Digite o numero da cidade (1, 2, 3, 4): \n");
+scanf("%d", &numeroCidade);
+printf("Numero da cidade: %d \n", numeroCidade);
 
-//numero da cidade A01
-printf("digite o numero da cidade(1,2,3 e 4): \n");
-scanf("%d",&numeroCidade);
-
-printf("numero da cidade:%d \n",numeroCidade);
-
-//nome da cidade A01
-printf("digite o nome da cidade: \n");
+// Nome da cidade A01
+printf("Digite o nome da cidade: \n");
 scanf("%s", nome);
+printf("Nome da cidade: %s \n", nome);
 
-printf("nome da cidade:%s \n",nome);
+// Populacao da cidade A01
+printf("Digite a populacao da cidade: \n");
+scanf("%d", &populacao);
+printf("Populacao da cidade: %d habitantes \n", populacao);
 
-//populacao da cidade A01
-printf("digite populacao da cidade: \n");
-scanf("%d",&populacao);
+// Area da cidade A01
+printf("Area da cidade (em km²): \n");
+scanf("%f", &area);
+printf("Area da cidade: %.2f km² \n", area);
 
-printf("populacao da cidade: %d habitantes \n",populacao);
+// PIB da cidade A01
+printf("PIB da cidade (em bilhoes): \n");
+scanf("%lf", &pib);
+printf("PIB da cidade: %.2lf bilhoes \n", pib);
 
-//area da cidade A01
-printf("area da cidade em : \n");
-scanf("%.2f",&area);
+// Pontos turisticos da cidade A01
+printf("Pontos turisticos da cidade: \n");
+scanf("%d", &pontosTuristicos);
+printf("Pontos turisticos da cidade: %d \n", pontosTuristicos);
 
-printf("area da cidade:%.2ff km \n",area);
-
-//pib da cidade A01
-printf("pib da cidade: \n");
-scanf("%lf",&pib);
-
-printf("pib da cidade:%lf bilhoes\n",pib);
-
-//pontos turisticos da cidade A01
-printf("pontosturisticos da cidade: \n");
-scanf("%d",&pontosTuristicos);
-
-printf("pontos turisticos da cidade:%d \n", pontosTuristicos);
-
-//carta 2
-
+// Carta 2
 printf("***CIDADE 2***: \n");
 
-//codigo cidade A02
-printf("digite codigo da cidade(A,B,C,...H): \n");
-scanf("%c",&codigoEstado);
+// Codigo da cidade A02
+printf("Digite o codigo da cidade (A, B, C, ..., H): \n");
+scanf(" %c", &codigoEstado); 
+printf("Codigo da cidade: %c \n", codigoEstado);
 
-printf("codigo da cidade:%c \n",codigoEstado);
+// Numero da cidade A02
+printf("Digite o numero da cidade (1, 2, 3, 4): \n");
+scanf("%d", &numeroCidade);
+printf("Numero da cidade: %d \n", numeroCidade);
 
-//numero da cidade A02
-printf("digite o numero da cidade(1,2,3 e 4): \n");
-scanf("%d",&numeroCidade);
+// Nome da cidade A02
+printf("Digite o nome da cidade: \n");
+scanf("%s", nome);
+printf("Nome da cidade: %s \n", nome);
 
-printf("numero da cidade:%d \n",numeroCidade);
+// Populacao da cidade A02
+printf("Digite a populacao da cidade: \n");
+scanf("%d", &populacao);
+printf("Populacao da cidade: %d habitantes \n", populacao);
 
-//nome da cidade A02
-printf("digite o nome da cidade: \n");
-scanf("%s",nome);
+// Area da cidade A02
+printf("Area da cidade (em km²): \n");
+scanf("%f", &area); 
+printf("Area da cidade: %.2f km² \n", area);
 
-printf("nome da cidade:%s \n",nome);
+// PIB da cidade A02
+printf("PIB da cidade (em bilhoes): \n");
+scanf("%lf", &pib);
+printf("PIB da cidade: %.2lf bilhoes \n", pib);
 
-//populacao da cidade A02
-printf("digite populacao da cidade: \n");
-scanf("%d",&populacao);
-
-printf("populacao da cidade: %d habitantes \n",populacao);
-
-//area da cidade A02
-printf("area da cidade em : \n");
-scanf("%.2f",&area);
-
-printf("area da cidade:%.2f km \n",area);
-
-//pib da cidade A02
-printf("pib da cidade: \n");
-scanf("%lf",&pib);
-
-printf("pib da cidade:%lf bilhoes\n",pib);
-
-//pontos turisticos da cidade A02
-printf("pontosturisticos da cidade: \n");
-scanf("%d",&pontosTuristicos);
-
-printf("pontos turisticos da cidade:%d \n", pontosTuristicos);
+// Pontos turisticos da cidade A02
+printf("Pontos turisticos da cidade: \n");
+scanf("%d", &pontosTuristicos);
+printf("Pontos turisticos da cidade: %d \n", pontosTuristicos);
 
 return 0;
 };
